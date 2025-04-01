816768a7a3268d5aeca7a179cd13360c666c6508
JahdielArciniegas <arciniegasjahdiel@gmail.com>
2025-04-01 19:33:34 -0400

    Example 8.7

--- index.js
+++ index.js
@@ -29,14 +29,6 @@ let authors = [
 ];
 
 /*
- * Suomi:
- * Saattaisi olla järkevämpää assosioida kirja ja sen tekijä tallettamalla kirjan yhteyteen tekijän nimen sijaan tekijän id
- * Yksinkertaisuuden vuoksi tallennamme kuitenkin kirjan yhteyteen tekijän nimen
- *
- * English:
- * It might make more sense to associate a book with its author by storing the author's id in the context of the book instead of the author's name
- * However, for simplicity, we will store the author's name in connection with the book
- *
  * Spanish:
  * Podría tener más sentido asociar un libro con su autor almacenando la id del autor en el contexto del libro en lugar del nombre del autor
  * Sin embargo, por simplicidad, almacenaremos el nombre del autor en conexión con el libro
@@ -121,6 +113,10 @@ const typeDefs = `
       author: String!
       genres: [String!]!
     ): Books
+    editAuthor(
+      name: String!
+      setBornTo: Int!
+      ):Authors
   }
 
   type Query {
@@ -168,6 +164,18 @@ const resolvers = {
       }
       books = books.concat(book);
       return book;
+    },
+    editAuthor: (root, args) => {
+      const author = authors.find((author) => author.name === args.name);
+      console.log(author.name);
+      if (!author) {
+        return null;
+      }
+      const updatedAuthor = { ...author, born: args.setBornTo };
+      authors = authors.map((author) =>
+        author.name === args.name ? updatedAuthor : author
+      );
+      return updatedAuthor;
     },
   },
 };
